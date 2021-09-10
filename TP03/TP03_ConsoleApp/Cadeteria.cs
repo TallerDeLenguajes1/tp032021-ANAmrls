0c5db5f97119845a9184f3d8a81c9972704dc87c
Ana Lucia Morales <analuciamrls@gmail.com>
2021-09-10 12:18:04 -0300

    commit inicial

---
+++ TP03/TP03_ConsoleApp/Cadeteria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03_ConsoleApp
+{
+    public class Cadeteria
+    {
+        private string nombre;
+        private List<Cadete> cadetes;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
---
+++ TP03/TP03_ConsoleApp/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03_ConsoleApp
+{
+    public enum EstadoPedido
+    {
+        pendiente,
+        entregado
+    }
+    public class Pedido
+    {
+        private int nro;
+        private string obs;
+        private string cliente;
+        private EstadoPedido estado;
+
+        public int Nro { get => nro; set => nro = value; }
+        public string Obs { get => obs; set => obs = value; }
+        public string Cliente { get => cliente; set => cliente = value; }
+        public EstadoPedido Estado { get => estado; set => estado = value; }
+    }
+}
